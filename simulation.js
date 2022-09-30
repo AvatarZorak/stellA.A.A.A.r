4232429f3ef31200e6b72ad22c23ae77c4d92c50
AvatarZorak <alexhristov2005@gmail.com>
2022-09-30 14:36:05 +0300

    base of the site

---
+++ simulation.js
@@ -0,0 +1,14 @@
+function showMenuStars() {
+    document.getElementById('menu').style.display = "flex";
+    document.getElementById('close').style.display = "inline-block";
+ }
+
+ function showOptions() {
+    document.getElementById('options').style.display = "flex";
+ }
+
+ function closeMenu() {
+    document.getElementById('menu').style.display = "none";
+    document.getElementById('options').style.display = "none";
+    document.getElementById('close').style.display = "none";
+ }
