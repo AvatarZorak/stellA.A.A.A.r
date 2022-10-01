0e90de67d72feb30cd7706471b13c7b8b7379305
AvatarZorak <alexhristov2005@gmail.com>
2022-10-01 19:12:33 +0300

    random sized background dogs

--- simulation.js
+++ simulation.js
@@ -1,19 +1,19 @@
 function showMenuStars() {
-    document.getElementById('menu-stars').style.display = "flex";
-    document.getElementById('close-button').style.display = "inline-block";
-    document.getElementById('show-menu-stars').style.transform = "translateY(-120px)";
+   document.getElementById('menu-stars').style.display = "flex";
+   document.getElementById('close-button').style.display = "inline-block";
+   document.getElementById('show-menu-stars').style.transform = "translateY(-120px)";
  }
 
  function showOptions() {
-    document.getElementById('menu-parameters').style.display = "flex";
+   document.getElementById('menu-parameters').style.display = "flex";
  }
 
  function closeMenu() {
-    document.getElementById('menu-stars').style.display = "none";
-    document.getElementById('menu-parameters').style.display = "none";
-    document.getElementById('close-button').style.display = "none";
+   document.getElementById('menu-stars').style.display = "none";
+   document.getElementById('menu-parameters').style.display = "none";
+   document.getElementById('close-button').style.display = "none";
 
-    document.getElementById('show-menu-stars').style.transform = "translateY(0px)";
+   document.getElementById('show-menu-stars').style.transform = "translateY(0px)";
  }
 
 var i = 100;
@@ -34,15 +34,16 @@ function generateStars() {
    for(var i = 0; i < 100; i++) {
       var image = new Image();
       var environment = document.getElementById('wholepage')
-
-      console.log(window.screen.height, window.screen.width);
-
+      var width_height_size = `${randomInt(6, 20)}px`;
+      
       image.src = 'dog.png';
-      image.setAttribute('id', 'background-stars')
+      image.setAttribute('id', 'background-stars');
+
       image.style.top = `${randomInt(0, 95)}vh`;
       image.style.left = `${randomInt(0, 95)}vw`;
 
-
+      image.style.width = width_height_size;
+      image.style.height = width_height_size;
 
       environment.appendChild(image);
    }
