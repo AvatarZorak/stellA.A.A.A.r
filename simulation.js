4455a899e8b2d6fabb73ca67b958d93f7b7c3647
unknown <axitodorov@gmail.com>
2022-10-01 19:05:54 +0300

    More cleanup

--- simulation.js
+++ simulation.js
@@ -1,20 +1,19 @@
 function showMenuStars() {
-    document.getElementById('menu-stars').style.display = "flex";
-    document.getElementById('close-button').style.display = "inline-block";
-    document.getElementById('show-menu-stars').style.transform = "translateY(-120px)";
- }
+  document.getElementById('menu-stars').style.display = "flex";
+  document.getElementById('close-button').style.display = "inline-block";
+  document.getElementById('show-menu-stars').style.transform = "translateY(-120px)";
+}
 
- function showOptions() {
-    document.getElementById('menu-parameters').style.display = "flex";
- }
+function showOptions() {
+  document.getElementById('menu-parameters').style.display = "flex";
+}
 
- function closeMenu() {
-    document.getElementById('menu-stars').style.display = "none";
-    document.getElementById('menu-parameters').style.display = "none";
-    document.getElementById('close-button').style.display = "none";
-
-    document.getElementById('show-menu-stars').style.transform = "translateY(0px)";
- }
+function closeMenu() {
+  document.getElementById('menu-stars').style.display = "none";
+  document.getElementById('menu-parameters').style.display = "none";
+  document.getElementById('close-button').style.display = "none";
+  document.getElementById('show-menu-stars').style.transform = "translateY(0px)";
+}
 
 var i = 100;
 
@@ -27,23 +26,19 @@ setInterval( () => {
 }, 100);
 
 function randomInt(min, max) {
-   return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 function generateStars() {
-   for(var i = 0; i < 100; i++) {
-      var image = new Image();
-      var environment = document.getElementById('wholepage')
+  for(var i = 0; i < 100; i++) {
+    var image = new Image();
+    var environment = document.getElementById('wholepage')
 
-      console.log(window.screen.height, window.screen.width);
+    image.src = 'dog.png';
+    image.setAttribute('id', 'background-stars')
+    image.style.top = `${randomInt(0, 95)}vh`;
+    image.style.left = `${randomInt(0, 95)}vw`;
 
-      image.src = 'dog.png';
-      image.setAttribute('id', 'background-stars')
-      image.style.top = `${randomInt(0, 95)}vh`;
-      image.style.left = `${randomInt(0, 95)}vw`;
-
-
-
-      environment.appendChild(image);
-   }
+    environment.appendChild(image);
+  }
 }
